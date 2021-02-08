b45911ffafb72099c376596574388ac08832b62d
dpetrova <daniella_petrova@abv.bg>
2021-02-08 12:44:02 +0200

    working with files, support drag-n-drop of files, alert user before discarding unsaved changes

--- 2.2.markdown-editor_(multiple-windows)/app/main.js
+++ 2.2.markdown-editor_(multiple-windows)/app/main.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 
 //create a Set to keep track of new windows
 const windows = new Set()
+//create a Map to watch currently opened files
+const openFiles = new Map()
 
 //create a window when the application is ready
 app.on('ready', () => {
@@ -25,6 +27,17 @@ app.on('activate', (event, hasVisibleWindows) => {
   if (!hasVisibleWindows) {
     createWindow()
   }
+})
+
+//responding to external requests to open a file with the application (e.g. open file from recent documents)
+app.on('will-finish-launching', () => {
+  app.on('open-file', (event, file) => {
+    event.preventDefault()
+    const win = createWindow()
+    win.once('ready-to-show', () => {
+      openFile(win, file)
+    })
+  })
 })
 
 //create new window
@@ -53,22 +66,49 @@ const createWindow = () => {
     },
   })
 
+  //implement isEdited property to can track if currently open file is edited
+  newWindow.isCurrentlyEdited = false
+
   newWindow.loadFile('app/index.html')
   //newWindow.webContents.loadURL(`file://${__dirname}/index.html`) //url must contain the protocol prefix, e.g. the http:// or file://
-
-  //add the window to the windows set when it has been opened
-  windows.add(newWindow)
 
   newWindow.once('ready-to-show', () => {
     newWindow.show()
   })
 
+  //closed event is fired when the window has successfully been closed
   newWindow.on('closed', () => {
     //remove the reference from the windows set when it has been closed
     windows.delete(newWindow)
+    //when the window is closed, stops the watcher for the file associated with that window
+    stopWatchingFile(newWindow)
     newWindow = null
   })
 
+  //close event is fired when the user attempts to close the window
+  newWindow.on('close', (event) => {
+    //check if the document has been edited (we set this in the renderer process on every keyup in the Markdown view by comparing the current content with the original content)
+    if (newWindow.isDocumentEdited() || newWindow.isCurrentlyEdited) {
+      //if the window has unsaved changes, prevents it from closing
+      event.preventDefault()
+
+      //prompt the user with a custom message box about unsaved changes; save their selection into “result”
+      const result = dialog.showMessageBoxSync(newWindow, {
+        type: 'warning',
+        title: 'Quit with Unsaved Changes?',
+        message: 'Your changes will be lost permanently if you do not save.',
+        buttons: ['Quit Anyway', 'Cancel'], //provides a list of button labels
+        defaultId: 0, //set the first button as the default option if the user hits the Return key
+        cancelId: 1, //set the second button as the button selected if the user dismisses the message box
+      })
+
+      //showMessageBox() returns the index of the button that the user selected -> if the user selects “Quit Anyway”, forces the window to close
+      if (result === 0) newWindow.destroy()
+    }
+  })
+
+  //add the window to the windows set when it has been opened
+  windows.add(newWindow)
   return newWindow
 }
 
@@ -95,11 +135,79 @@ const getFileFromUser = (targetWindow) => {
 const openFile = (targetWindow, file) => {
   //read from the file, and converts the resulting buffer to a string
   const content = fs.readFileSync(file).toString()
+  //append file to operating system’s list of recently opened documents
+  app.addRecentDocument(file)
+  //set the represented file in macOS
+  targetWindow.setRepresentedFilename(file)
   //send the name of the file and its content to the specific window in the renderer process over the “file-opened” channel
   targetWindow.webContents.send('file-opened', file, content)
+  //start watching that file for changes be another application
+  startWatchingFile(targetWindow, file)
+}
+
+//export the generated html output
+const saveHtml = (targetWindow, content) => {
+  const file = dialog.showSaveDialogSync(targetWindow, {
+    title: 'Save HTML',
+    defaultPath: app.getPath('documents'), //defaults to the user’s “documents” directory as defined by the operating system
+    filters: [{ name: 'HTML Files', extensions: ['html', 'htm'] }],
+  })
+
+  //if the user selects cancel in the File dialog box, aborts the function
+  if (!file) return
+
+  fs.writeFileSync(file, content)
+}
+
+//saving the current markdown file
+const saveMarkdown = (targetWindow, file, content) => {
+  //if this is a new file without a file path, prompts the user to select a file path with a dialog box
+  if (!file) {
+    file = dialog.showSaveDialogSync(targetWindow, {
+      title: 'Save Markdown',
+      defaultPath: app.getPath('documents'),
+      filters: [{ name: 'Markdown Files', extensions: ['md', 'markdown'] }],
+    })
+  }
+
+  //if the user selects Cancel in the File dialog box, aborts the function
+  if (!file) return
+
+  //write the contents of the buffer to the filesystem
+  fs.writeFileSync(file, content)
+  openFile(targetWindow, file)
+}
+
+//start watching a file path when it’s opened
+const startWatchingFile = (targetWindow, file) => {
+  //close the existing watcher if there is one
+  stopWatchingFile(targetWindow)
+
+  //fire file-changed if external app change currently opened file
+  const watcher = fs.watch(file, (eventType, filename) => {
+    if (eventType === 'change') {
+      const content = fs.readFileSync(file).toString()
+      targetWindow.webContents.send('file-changed', file, content)
+    }
+  })
+
+  //track the watcher so we can stop it later
+  openFiles.set(targetWindow, watcher)
+}
+
+//stop watching a file when either the window has been closed or the user opens a different file in the window
+const stopWatchingFile = (targetWindow) => {
+  //check if we have a watcher running for this window
+  if (openFiles.has(targetWindow)) {
+    openFiles.get(targetWindow).close() //stop watcher
+    openFiles.delete(targetWindow) //delete the watcher from the maps of open windows
+  }
 }
 
 //еxport ability to open the file dialog from the renderer process
 exports.getFileFromUser = getFileFromUser
 //еxport ability to create new window from the renderer process
 exports.createWindow = createWindow
+exports.openFile = openFile
+exports.saveHtml = saveHtml
+exports.saveMarkdown = saveMarkdown
--- 2.2.markdown-editor_(multiple-windows)/app/renderer.js
+++ 2.2.markdown-editor_(multiple-windows)/app/renderer.js
@@ -3,6 +3,7 @@
 const { remote, ipcRenderer } = require('electron')
 //low-level compiler for parsing markdown
 const marked = require('marked')
+const path = require('path')
 
 //remote module has its own require method that allows it to require functionality from the main process in our renderer process
 const mainProcess = remote.require('./main.js') //require the main process
@@ -21,18 +22,25 @@ const saveHtmlButton = document.querySelector('#save-html')
 const showFileButton = document.querySelector('#show-file')
 const openInDefaultButton = document.querySelector('#open-in-default')
 
+//declaring global variables for keeping track of the current working file
+let filePath = null
+let originalContent = ''
+
+const isDifferentContent = (content) => content !== markdownView.value
+
 //convert Markdown to HTML
 const renderMarkdownToHtml = (markdown) => {
   htmlView.innerHTML = marked(markdown, { sanitize: true })
 }
 
-//re-render the HTML when Markdown changes
+//checking for changes whenever the user types and re-render the HTML when Markdown changes
 markdownView.addEventListener('keyup', (event) => {
   const currentContent = event.target.value
   renderMarkdownToHtml(currentContent)
+  updateUserInterface(currentContent !== originalContent)
 })
 
-//triggering getFileFromUser() in the main process from the UI as pass a reference to the current window
+//triggering Open File dialog in the main process
 openFileButton.addEventListener('click', () => {
   mainProcess.getFileFromUser(currentWindow)
 })
@@ -42,8 +50,148 @@ newFileButton.addEventListener('click', () => {
   mainProcess.createWindow()
 })
 
+//triggering the Save File dialog box from the main process
+saveHtmlButton.addEventListener('click', () => {
+  mainProcess.saveHtml(currentWindow, htmlView.innerHTML)
+})
+
+saveMarkdownButton.addEventListener('click', () => {
+  mainProcess.saveMarkdown(currentWindow, filePath, markdownView.value)
+})
+
+//reverting markdown content of to last saved content
+revertButton.addEventListener('click', () => {
+  markdownView.value = originalContent
+  renderMarkdownToHtml(originalContent)
+})
+
 //listening for messages on the "file-opened" channel
 ipcRenderer.on('file-opened', (event, file, content) => {
+  //prompt user of insaved changes in previous opened file
+  if (
+    (currentWindow.isDocumentEdited() || currentWindow.isCurrentlyEdited) &&
+    isDifferentContent(content)
+  ) {
+    //use the remote module to trigger the dialog box from the main process
+    const result = remote.dialog.showMessageBoxSync(currentWindow, {
+      type: 'warning',
+      title: 'Overwrite Current Unsaved Changes?',
+      message:
+        'Opening a new file in this window will overwrite your unsaved changes. Open this file anyway?',
+      buttons: ['Yes', 'Cancel'],
+      defaultId: 0,
+      cancelId: 1,
+    })
+
+    //if the user cancels, returns from the function early
+    if (result === 1) return
+  }
+
+  //render new opened file
+  renderFile(file, content)
+})
+
+ipcRenderer.on('file-changed', (event, file, content) => {
+  if (!isDifferentContent(content)) return
+  const result = remote.dialog.showMessageBoxSync(currentWindow, {
+    type: 'warning',
+    title: 'Overwrite Current Unsaved Changes?',
+    message: 'Another application has changed this file. Load changes?',
+    buttons: ['Yes', 'Cancel'],
+    defaultId: 0,
+    cancelId: 1,
+  })
+
+  if (result === 1) return
+
+  renderFile(file, content)
+})
+
+const renderFile = (file, content) => {
+  //update path and original content of currently open file
+  filePath = file
+  originalContent = content
+
+  //update the Markdown content in the UI
   markdownView.value = content
+  //update the HTML content in the UI
   renderMarkdownToHtml(content)
+  //update the window’s title bar whenever a new file is opened
+  updateUserInterface(false)
+}
+
+const updateUserInterface = (isEdited) => {
+  let title = 'Fire Sale'
+
+  //if a file is open, prepends the name of that file to the title
+  if (filePath) {
+    title = `${path.basename(filePath)} - ${title}`
+  }
+  //if document has unsaved changes
+  if (isEdited) {
+    title = `${title} (Edited)`
+  }
+
+  //update the title of the window
+  currentWindow.setTitle(title)
+  //if isEdited is true, then updates the window accordingly
+  currentWindow.setDocumentEdited(isEdited) //work only for macOS
+  currentWindow.isCurrentlyEdited = isEdited //change our implementation of tacking rditing of file
+
+  //if the document is not edited, disable Save and revert buttons
+  saveMarkdownButton.disabled = !isEdited
+  revertButton.disabled = !isEdited
+}
+
+/* Implement Drag and Drop */
+
+//disable the default browser behavior
+document.addEventListener('dragstart', (event) => event.preventDefault())
+document.addEventListener('dragover', (event) => event.preventDefault())
+document.addEventListener('dragleave', (event) => event.preventDefault())
+document.addEventListener('drop', (event) => event.preventDefault())
+
+//when a user is dragging a file, we have access only to its metadata
+const getDraggedFile = (event) => event.dataTransfer.items[0]
+//after the user has officially dropped the file, we have access to the file itself
+const getDroppedFile = (event) => event.dataTransfer.files[0]
+//checks to see if the type of file being dragged is either of the two types supported by the application
+const fileTypeIsSupported = (file) => {
+  return ['text/plain', 'text/markdown'].includes(file.type)
+}
+
+//adding and removing classes on dragover and dragleave to give the user a visual clue as to whether the drop is going to be successful
+markdownView.addEventListener('dragover', (event) => {
+  const file = getDraggedFile(event)
+
+  if (fileTypeIsSupported(file)) {
+    //if the file type is supported, adds a CSS class to indicate this is a valid place to drop the file
+    markdownView.classList.add('drag-over')
+  } else {
+    //if the file type is not supported, adds a CSS class to indicate that this file is not accepted
+    markdownView.classList.add('drag-error')
+  }
+})
+
+//when the user removes the file from the target area, we’ll clean up any classes that were added and restore the UI to its default state
+markdownView.addEventListener('dragleave', () => {
+  markdownView.classList.remove('drag-over')
+  markdownView.classList.remove('drag-error')
+})
+
+//open dropped file
+markdownView.addEventListener('drop', (event) => {
+  const file = getDroppedFile(event)
+
+  if (fileTypeIsSupported(file)) {
+    //if the file type is supported, the renderer process communicates with the main process to open it
+    mainProcess.openFile(currentWindow, file.path)
+  } else {
+    //if the file type is not supported, the application alerts the user
+    alert('That file type is not supported')
+  }
+
+  //clean up any classes that were added and restore the UI to its default state
+  markdownView.classList.remove('drag-over')
+  markdownView.classList.remove('drag-error')
 })
